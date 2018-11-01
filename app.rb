ab061a0f3d3035f84bfc0e9ea7d2f6148bfac7d4
Israel Tomilayo <israel.tomilayo@andela.com>
2018-11-01 13:03:10 +0100

    setup router and allow rack to use puma webserver

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'yaml'
+ROUTES = YAML.load(File.read(File.join(File.dirname(__FILE__), "app", "routes.yml")))
+
+Dir[File.join(File.dirname(__FILE__), 'lib', '*.rb')].each {|file| require file }
+Dir[File.join(File.dirname(__FILE__), 'app', '**', '*.rb')].each {|file| require file }
+
+require "./lib/router"
+
+class App
+  attr_reader :router
+
+  def initialize
+    @router = Router.new(ROUTES)
+  end
+
+  def call(env)
+    result = router.resolve(env)
+    [result.status, result.headers, result.content]
+  end
+end
